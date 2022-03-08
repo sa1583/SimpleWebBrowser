63d7cde08ea23b2a7e16177b42d99634c1c3ea04
sa1583 <sa1583@naver.com>
2022-03-08 13:08:55 +0900

    basic function

--- app/src/main/java/fastcampus/aop/part2/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/simplewebbrowser/MainActivity.kt
@@ -1,11 +1,46 @@
 package fastcampus.aop.part2.simplewebbrowser
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { view, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(view.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
     }
 }
